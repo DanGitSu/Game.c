c1a8a2ae138f2ffb6b3cb2a5a32df6e8aca4a412
Jason Yu <jasonyu0100@gmail.com>
2018-04-20 20:52:46 +1000

    values

--- testGame.c
+++ testGame.c
@@ -3,8 +3,18 @@
 #include <stdlib.h>
 #include <assert.h>
 
+#define DEFAULT_DISCIPLINES {STUDENT_BQN, STUDENT_MMONEY, STUDENT_MJ, \
+                STUDENT_MMONEY, STUDENT_MJ, STUDENT_BPS, STUDENT_MTV, \
+                STUDENT_MTV, STUDENT_BPS,STUDENT_MTV, STUDENT_BQN, \
+                STUDENT_MJ, STUDENT_BQN, STUDENT_THD, STUDENT_MJ, \
+                STUDENT_MMONEY, STUDENT_MTV, STUDENT_BQN, STUDENT_BPS}
+
+#define DEFAULT_DICE {9,10,8,12,6,5,3,11,3,11,4,6,4,7,9,2,8,10,5}
+
 void tests(void)
 
 int main() {
-	return EXIT_SUCCESS
+
+
+	return EXIT_SUCCESS;
 }
